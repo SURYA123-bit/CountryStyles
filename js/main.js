5204e462c44f008e653e7896b13e537b616eb145
SURYA123-bit <surya.pasumarthi111@gmail.com>
2020-06-13 18:39:01 -0400

    First Commit

---
+++ js/main.js
@@ -0,0 +1,22 @@
+$(document).ready(function(){
+
+
+	/*  Hamburger Menu & Icon  */
+	$('.hamburger').on('click', function(e){
+		
+		e.preventDefault();
+		$(this).toggleClass('opned');
+		$('header nav').toggleClass('active');
+		
+	});
+
+
+
+
+	var dt = new Date();
+document.getElementById("datetime").innerHTML = (("0"+dt.getDate()).slice(-2)) +"."+ (("0"+(dt.getMonth()+1)).slice(-2)) +"."+ (dt.getFullYear()) +" "+ (("0"+dt.getHours()).slice(-2)) +":"+ (("0"+dt.getMinutes()).slice(-2));
+
+
+
+
+});
